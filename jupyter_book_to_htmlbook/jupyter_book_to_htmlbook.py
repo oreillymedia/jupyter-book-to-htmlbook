04bc6410c56d6a7ebdff64627866b14973680ec5
Danny Elfanbaum <delfanbaum@oreilly.com>
2022-08-03 11:47:20 -0400

    add testing infrastructure and table of contents happy-path tests

--- jupyter_book_to_htmlbook/jupyter_book_to_htmlbook.py
+++ jupyter_book_to_htmlbook/jupyter_book_to_htmlbook.py
@@ -1,18 +1,19 @@
-from toc_processing import *
-from chapter_processing import *
+from toc_processing import get_book_index
+from chapter_processing import process_chapter
 import os
 from pathlib import Path
 
-# default html directory
-html_dir = 'html/'
+# default source html directory
+html_dir = Path('html/')
 
 # default build directory
 build_dir = Path('build/')
 image_dir = build_dir / 'images'
 
+
 def main():
     image_dir.mkdir(parents=True, exist_ok=True)
-    os.system(f'cp {html_dir}/_images/* {image_dir}') # note: no windows support
+    os.system(f'cp {html_dir}/_images/* {image_dir}')
     toc = get_book_index(html_dir)
     for element in toc:
         process_chapter(element, build_dir)
