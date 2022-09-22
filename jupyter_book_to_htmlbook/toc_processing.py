e2f962faa527b62fe1d873536f12bb1948a0f4f3
Danny Elfanbaum <delfanbaum@oreilly.com>
2022-09-22 11:48:28 -0400

    add support for parts in toc and use that as default for testing
    use as default for testing since there is better coverage elsewhere for bare chapters

--- jupyter_book_to_htmlbook/toc_processing.py
+++ jupyter_book_to_htmlbook/toc_processing.py
@@ -8,6 +8,20 @@ except ImportError:
     from yaml import SafeLoader
 
 
+def _path(file_stub, src_dir):
+    return Path(src_dir / f'_build/html/{file_stub}.html')
+
+
+def _log_print_and_exit(message):
+    """
+    helper function to log a script-ending error message, print it,
+    and exit with an error code indicating failure
+    """
+    logging.error(message)
+    print(message)
+    sys.exit(1)
+
+
 def get_book_toc(src_dir: Path) -> list:
     """
     Takes the 'genindex' file from a Jupyter Book HTML site and returns a
@@ -16,45 +30,77 @@ def get_book_toc(src_dir: Path) -> list:
     """
     compiled_toc = []
 
-    def _path(file_stub, src_dir=src_dir):
-        return Path(src_dir / f'_build/html/{file_stub}.html')
-
     try:
         with open(src_dir / "_toc.yml") as f:
             toc = load(f.read(), SafeLoader)
             # exit if we see some format other than jb-book
             try:
                 if toc["format"] != "jb-book":
-                    logging.error("Unsupported jupyter book format: " +
-                                  toc["format"] + ". The only supported" +
-                                  " format is 'jb-book'.")
-                    sys.exit()
+                    message = ("Unsupported jupyter book format: " +
+                               toc["format"] + ". The only supported" +
+                               " format is 'jb-book'.")
+                    _log_print_and_exit(message)
             except KeyError:
-                logging.error("Malformed _toc.yml file. Please see " +
-                              "jupyterbook.org for correct syntax.")
-                sys.exit()
+                message = ("Malformed _toc.yml file. Please see " +
+                           "jupyterbook.org for correct syntax.")
+                _log_print_and_exit(message)
 
             # add "root"
-            compiled_toc.append(_path(toc["root"]))
+            compiled_toc.append(_path(toc["root"], src_dir))
 
-            # process chapters
-            for chapter in toc['chapters']:
-                try:  # if chapter has sections
-                    chapter_files: list[Path] = []
-                    # append first file
-                    chapter_files.append(_path(chapter['file']))
-
-                    for section in chapter["sections"]:
-                        chapter_files.append(_path(section['file']))
-
-                    compiled_toc.append(chapter_files)
-
-                except KeyError:  # chapter _doesn't_ have sections
-                    compiled_toc.append(_path(chapter['file']))
+            if "parts" in toc.keys():
+                compiled_toc.extend(process_parts(toc["parts"], src_dir))
+            else:
+                compiled_toc.extend(process_chapters(toc["chapters"], src_dir))
 
     except FileNotFoundError:
-        logging.error("Can't find the _toc.yml file. Ensure you're " +
-                      "specifying a valid jupyter book project as the SOURCE.")
-        sys.exit()
+        message = "Can't find the _toc.yml file. Ensure you're " + \
+                  "specifying a valid jupyter book project as the SOURCE."
+        _log_print_and_exit(message)
 
     return compiled_toc
+
+
+def process_chapters(chapter_list, src_dir) -> list:
+    """
+    takes a `chapters:` list, optionally with sections
+    and returns a list (or list of) of Paths
+    """
+    chapters = []
+    for chapter in chapter_list:
+        try:  # if chapter has sections
+            chapter_files: list[Path] = []
+            # append first file
+            chapter_files.append(_path(chapter['file'], src_dir))
+
+            for section in chapter["sections"]:
+                chapter_files.append(_path(section['file'], src_dir))
+
+            chapters.append(chapter_files)
+
+        except KeyError:  # chapter _doesn't_ have sections
+            chapters.append(_path(chapter['file'], src_dir))
+
+    return chapters
+
+
+def process_parts(parts, src_dir):
+    """ part processing function"""
+    part_number = 0
+    part_files = []
+    for part in parts:
+        part_number += 1  # increment numbering
+        # create a distinct placeholder Path for downstream processing
+        try:
+            part_title = part["caption"].replace(' ', '-').lower()
+        except TypeError:  # because it's looking for a string, getting an int
+            message = ("Missing part caption in _toc.yml. " +
+                       "Part captions are required.")
+            _log_print_and_exit(message)
+        part_stub = f'_jb_part-{part_number}-{part_title}'
+        part_files.append(_path(part_stub, src_dir))
+        # now add the chapters
+        part_files.extend(
+            process_chapters(part["chapters"], src_dir)
+        )
+    return part_files
