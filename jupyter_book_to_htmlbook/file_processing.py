830cf14444cedebfc4b1d84011c7225f88f5635a
Danny Elfanbuam <delfanbaum@oreilly.com>
2022-12-14 11:27:17 -0500

    Refactor text processing out of file_processing
    This is to set us up for future work, but also it doesn't really make
    sense to have the span movement and "cleans" done in the file processing
    file, since those are really text tasks, not file tasks. This just moves
    those functions into a new file and moves the tests around (and also
    renames the test_chapter test file, which really should have been
    renamed test_file for a while now anyway.

--- jupyter_book_to_htmlbook/file_processing.py
+++ jupyter_book_to_htmlbook/file_processing.py
@@ -8,6 +8,10 @@ from .footnote_processing import process_footnotes
 from .math_processing import process_math
 from .xref_processing import process_interal_refs, process_ids
 from .code_processing import process_code
+from .text_processing import (
+        clean_chapter,
+        move_span_ids_to_sections
+    )
 
 
 def process_part(part_path: Path, output_dir: Path):
@@ -168,62 +172,6 @@ def compile_chapter_parts(ordered_chapter_files_list):
         subsection = process_chapter_subparts(subfile)
         chapter.append(subsection)  # type: ignore
 
-    return chapter
-
-
-def clean_chapter(chapter, rm_numbering=True):
-    """
-    "Cleans" the chapter from any script or style tags, removes table borders,
-    removes any style attrs, and by default removes any section numbering.
-    """
-    remove_tags = ['style', 'script']
-    all_tags = chapter.find_all()
-    for tag in all_tags:
-        if tag.name in remove_tags:
-            tag.decompose()
-        if tag.name == 'table':
-            del tag['border']
-    for tag in chapter.find_all(attrs={'style': True}):
-        del tag['style']
-
-    # (optionally) remove numbering
-    if rm_numbering:
-        for span in chapter.find_all(class_="section-number"):
-            span.decompose()
-
-    classes_to_remove = [
-        # remove hidden cells. in the web version, these cells are hidden by
-        # default and users can toggle them on/off. but they take up too much
-        # space if rendered into the pdf.
-        ".tag_hide-input > .cell_input",
-        ".tag_hide-output > .cell_output",
-        ".tag_hide-cell",
-        ".toggle-details",
-
-        # remove any heading links
-        ".headerlink",
-    ]
-
-    for el in chapter.select(','.join(classes_to_remove)):
-        el.decompose()
-    return chapter
-
-
-def move_span_ids_to_sections(chapter):
-    """
-    Takes span tags with "sec-" ids and moves the id to the parent section tag
-    so Atlas can find the cross reference.
-    """
-    sec_spans = chapter.find_all("span", id=re.compile('sec-*'))
-    for span in sec_spans:
-        # get id
-        span_id = span['id']
-        # get next heading
-        section = span.find_parent('section')
-        # add span id to section
-        section['id'] = span_id
-        # remove span so no dup ids
-        span.decompose()
     return chapter
 
 
---
+++ tests/test_text_processing.py
@@ -0,0 +1,58 @@
+from bs4 import BeautifulSoup  # type: ignore
+from jupyter_book_to_htmlbook.text_processing import (
+    clean_chapter,
+    move_span_ids_to_sections
+    )
+
+
+def test_chapter_cleans():
+    """ test that we're ripping out the things we want to """
+    chapter_text = r"""<style>body: 13px</style>
+<script src="js/js.js" />
+<table border="7">
+<tr><td>Hello</td><td>World</td></tr>
+</table>
+<p style="text-decoration: underline">Lorem ipsum</p>
+<h2><span class="section-number">19.1.1.
+</span>Issues with Linear Regression<a class="headerlink"
+href="#issues-with-linear-regression" title="Permalink to this headline">¶</a>
+/h2>
+<div class="cell tag_hide-input docutils container">
+div class="cell_input docutils container">
+<pre> some thing </pre>
+</div>
+</div>"""
+    chapter = BeautifulSoup(chapter_text, 'html.parser')
+    result = clean_chapter(chapter)
+    assert str(result) == """
+
+<table>
+<tr><td>Hello</td><td>World</td></tr>
+</table>
+<p>Lorem ipsum</p>
+<h2>Issues with Linear Regression
+/h2&gt;
+<div class="cell tag_hide-input docutils container">
+div class="cell_input docutils container"&gt;
+<pre> some thing </pre>
+</div>
+</h2>"""
+
+
+def test_move_span_ids_to_sections():
+    """
+    Atlas requires that cross reference targets sections so that
+    the text will appear as expected. This test is to confirm that
+    the ids we added ("sec-") to the invisible spans earlier for cross
+    referencing are then applied to the parent section.
+    """
+    chapter_text = """
+<section class="section" data-type="sect2" id="types-of-bias">
+<span id="sec-biastypes"></span><h2>Types of Bias</h2>
+<p>Bias comes in many forms!</p>"""
+    chapter = BeautifulSoup(chapter_text, 'html.parser')
+    result = move_span_ids_to_sections(chapter)
+    assert str(result) == """
+<section class="section" data-type="sect2" id="sec-biastypes">
+<h2>Types of Bias</h2>
+<p>Bias comes in many forms!</p></section>"""
